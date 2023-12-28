38daa65ea7dfda34f8c3dda7ffdd542b665de7fe
evairson <eva.herson@me.com>
2023-12-28 21:23:33 +0100

    add new form (title color and icon)

---
+++ HealthyMind/AddNewTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewTaskView.swift
+//  HealthyMind
+//
+//  Created by Eva Herson on 28/12/2023.
+//
+
+import SwiftUI
+
+struct AddNewTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewTaskView()
+}
